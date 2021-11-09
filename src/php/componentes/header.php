706a7651da22b219a531ca1ba28b4aac268553e7
marquin.e <marcos.emanuel.04@gmail.com>
2021-11-09 19:52:02 -0300

    Misturando café com leite

--- src/php/componentes/header.php
+++ src/php/componentes/header.php
@@ -1,27 +1,30 @@
+<!-- Header  -->
 <header class="header-navbar">
-        <nav id="navbar">
-            <img src="assets/img/Logo.svg" alt="Logo" id="nav-logo">
-            <div class="mobile-menu">
-                <div class="line1"></div>
-                <div class="line2"></div>
-                <div class="line3"></div>
-            </div>
-            <ul>
-                <li>
-                    <a href="#">Home</a>
-                </li>
-                <li>
-                    <a href="src/pages/new.php">Notícias</a>
-                </li>
-                <li>
-                    <a href="#newsletter">Newsletter</a>
-                </li>
-                <li>
-                    <a href="#">Nos Encontre</a>
-                </li>
-                <li>
-                    <a href="#footer-section">Serviços</a>
-                </li>
-            </ul>
-        </nav>
-    </header>
+    <nav id="navbar">
+        <img src="assets/img/Logo.svg" alt="Logo" id="nav-logo">
+        <div class="mobile-menu">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+        </div>
+        <ul class="nav-list">
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#news">Notícias</a>
+            </li>
+            <li>
+                <a href="#newsletter">Newsletter</a>
+            </li>
+            <li>
+                <a href="#">Nos Encontre</a>
+            </li>
+            <li>
+                <a href="#footer-section">Serviços</a>
+            </li>
+        </ul>
+    </nav>
+</header>
+
+<script src="/assets/js/mobile-navbar.js"></script>
