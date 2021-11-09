1208c07ce279c4f05df3bb7f8b3eb3ecd15c249d
marquin.e <marcos.emanuel.04@gmail.com>
2021-11-09 19:31:51 -0300

    Correção do header + estilos aplicados na section de newsletter

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="icon" href="assets/img/Logosemnome.svg">
     <script type="text/javascript" src="assets/js/NLAction.js"></script>
