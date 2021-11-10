eb5f60f7914dcc0b4eaba6387d1ac8e6689b8d32
marquin.e <marcos.emanuel.04@gmail.com>
2021-11-10 20:13:10 -0300

    Cortei o bolo em pedaços

---
+++ src/php/componentes/newsletterForm.php
@@ -0,0 +1,11 @@
+<hr class="divisorias">
+    <div class="receberNoticias">
+        <img class='logoReceberNoticias'src='https://www.sp.senac.br/o/senac-theme/images/logo_senac_default.png'>
+        <h1 class='textoGenerico'>Receba notícias via e-email e fique por dentro das novidades!</h1>
+        <div class="receberDadosUsuario">
+            <input class="inputNome" placeholder='Digite seu nome aqui'>
+            <input class="inputEmail"placeholder='Digite seu email aqui'>
+            <button class='botaoEnviar'>Enviar</button>
+        </div>
+    </div>
+<hr class="divisorias">
