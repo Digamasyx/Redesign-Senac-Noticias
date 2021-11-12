e2a8f016e63a9b61d01983a3866c3ee43d74560c
Renan Ramos <90880803+Ryotruuser@users.noreply.github.com>
2021-11-12 00:03:46 -0300

    transformação e adaptação
    transformado de tag para botões todos os itens do menu de acessibilidade, pagina news telas grandes padrão forbes finalizada, bug de compatibilidade no menu de acessibilidade (lightmode e alto contraste não se dão bem) resolução e simples porém vale a pena ? talvez seja redundante.

--- assets/js/menu-acessibilidade.js
+++ assets/js/menu-acessibilidade.js
@@ -54,6 +54,14 @@ function noBoldFont(){
 }
 function fontSizeDecrease(){
     var text = document.getElementById("texto-noticias");
+    if(window.matchMedia("(min-width: 1024px)").matches){   
+        if (text.style.fontSize == '3em'){
+            return text.style.fontSize = '2.5em';
+        }
+        else{
+            return text.style.fontSize = '1.9em';
+        }
+    }
     if (text.style.fontSize == '1.8em'){
         text.style.fontSize = '1.2em';
     }else{
@@ -62,37 +70,50 @@ function fontSizeDecrease(){
 }
 function fontSizeIncrease(){
     var text = document.getElementById("texto-noticias");
+    if(window.matchMedia("(min-width: 1024px)").matches){   
+        if (text.style.fontSize == '1.9em'){
+            return text.style.fontSize = '2.5em';
+        }
+        else{
+            return text.style.fontSize = '3em';
+        }
+    }
     if (text.style.fontSize == '0.8em'){
         text.style.fontSize = '1.2em';
     }else{
         text.style.fontSize = '1.8em';
     }
+    
 }
 function lightMode(){
     var main = document.getElementById("body-page");
     var accessMenu = document.getElementById("caixa-de-acessibilidade");
+    var luzBtn = document.getElementById("btn-iluminacao")
     var accessBtn = document.getElementsByClassName('botao-acessibilidade');
     var socialMediaMenu = document.getElementById('midia-social');
-    var imgNews = document.getElementById('acessibilidade-iluminacao');
     if (main.style.color == 'white'){
         for (var i = 0; i < accessBtn.length; i++) {
-            accessBtn[i].style.color = 'black';}
+            accessBtn[i].style.color = 'black';
+            accessBtn[i].style.background = 'white';}
         main.style.background = 'white';
         main.style.color = 'black';
         accessMenu.style.borderBottom = '2px solid black'; 
         accessMenu.style.borderTop = '2px solid black';
         socialMediaMenu.style.borderTop = '2px solid black';
         socialMediaMenu.style.borderBottom = '2px solid black';
-        imgNews.style.border = '1px solid black';
+        luzBtn.style.background = 'white';
     }else{
         for (var i = 0; i < accessBtn.length; i++) {
-            accessBtn[i].style.color = 'white';}
+            accessBtn[i].style.color = 'white';
+            accessBtn[i].style.background = '#0F0F0F';
+        }
         main.style.background = '';
         main.style.color = 'white';
         accessMenu.style.color = 'white';
         accessMenu.style.borderBottom = '2px solid white'; 
         accessMenu.style.borderTop = '2px solid white';
         socialMediaMenu.style.borderTop = '2px solid white';
-        socialMediaMenu.style.borderBottom = '2px solid white'; 
+        socialMediaMenu.style.borderBottom = '2px solid white';
+        luzBtn.style.background = '#0F0F0F'; 
     }
 }
