a203af6ea7d79be30f94e69a4f0eb31d600bfab3
Renan Ramos <90880803+Ryotruuser@users.noreply.github.com>
2021-11-17 16:27:34 -0300

    criada pagina de estilo da allNews.php
    Foi criada a pagina de estilo da allNews.php, que e a pagina onde vai ter todas as noticias, os cards no html são somente para ilustração até as noticias serem puxadas de fato do banco de dados, as classes se precisam ser dadas aos componentes e acredito que não teremos problemas para carregar a caixa de noticias a partir dai.

--- src/pages/allNews.php
+++ src/pages/allNews.php
@@ -10,6 +10,7 @@ require_once(dirname(__DIR__) . '/php/functions/getPath.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
     <link rel="icon" href="assets/img/Logosemnome.svg">
+    <script type="text/javascript" src="/assets/js/menu-acessibilidade.js"></script>
     <link rel="stylesheet" href="/assets/css/allNews.css">
     <title>SENAC | Ver notícias</title>
 </head>
@@ -17,12 +18,88 @@ require_once(dirname(__DIR__) . '/php/functions/getPath.php');
     <!-- Header  -->
     <?php require_once(dirname(__DIR__) . '/php/components/header.html'); ?>
 
-    <main>
-        <!-- News List -->
-        <?php require_once(dirname(__DIR__) . '/php/components/newsList.php'); ?>   
+    <main class='main'>
+    
+        <!-- noticia testa para definir o estilo css -->
+        <a class='noticia-caixa'href='#'>
+            <div class='main-noticias'>
+                <div class='noticias-imagem'>
+                    <img class='img-noticia' src='https://cdn.ome.lt/rMfE0BrA8n9jNU4fpI4Lig40p8A=/480x360/smart/filer_public/88/64/886430f8-1cb3-4ed3-b63f-574b7e06707f/spider_man_no_way_home_trailer_2.jpg'/>
+                </div>
+                <div class='noticias-cabecalho'>
+                    <h2 class='noticia-titulo'>‘Homem-Aranha: Sem Volta Para Casa’ ganha trailer faltando um mês para estreia</h2>
+                    <p class='noticia-descricao'>Para decepção de muitos, nada das versões anteriores do Homem-Aranha, interpretadas por...<p>
+                    <p class='leia-mais'>Leia mais...&rarr;</p>
+                </div>
+            </div>
+        </a>
+        <!-- fim noticia testa para definir o estilo css -->
+
+        <!-- noticia testa para definir o estilo css -->
+        <a class='noticia-caixa'href='#'>
+            <div class='main-noticias'>
+                <div class='noticias-imagem'>
+                    <img class='img-noticia' src='https://cdn.ome.lt/rMfE0BrA8n9jNU4fpI4Lig40p8A=/480x360/smart/filer_public/88/64/886430f8-1cb3-4ed3-b63f-574b7e06707f/spider_man_no_way_home_trailer_2.jpg'/>
+                </div>
+                <div class='noticias-cabecalho'>
+                    <h2 class='noticia-titulo'>‘Homem-Aranha: Sem Volta Para Casa’ ganha trailer faltando um mês para estreia</h2>
+                    <p class='noticia-descricao'>Para decepção de muitos, nada das versões anteriores do Homem-Aranha, interpretadas por...<p>
+                    <p class='leia-mais'>Leia mais...&rarr;</p>
+                </div>
+            </div>
+        </a>
+        <!-- fim noticia testa para definir o estilo css -->
+
+        <!-- noticia testa para definir o estilo css -->
+        <a class='noticia-caixa'href='#'>
+            <div class='main-noticias'>
+                <div class='noticias-imagem'>
+                    <img class='img-noticia' src='https://cdn.ome.lt/rMfE0BrA8n9jNU4fpI4Lig40p8A=/480x360/smart/filer_public/88/64/886430f8-1cb3-4ed3-b63f-574b7e06707f/spider_man_no_way_home_trailer_2.jpg'/>
+                </div>
+                <div class='noticias-cabecalho'>
+                    <h2 class='noticia-titulo'>‘Homem-Aranha: Sem Volta Para Casa’ ganha trailer faltando um mês para estreia</h2>
+                    <p class='noticia-descricao'>Para decepção de muitos, nada das versões anteriores do Homem-Aranha, interpretadas por...<p>
+                    <p class='leia-mais'>Leia mais...&rarr;</p>
+                </div>
+            </div>
+        </a>
+        <!-- fim noticia testa para definir o estilo css -->
+
+        <!-- noticia testa para definir o estilo css -->
+        <a class='noticia-caixa'href='#'>
+            <div class='main-noticias'>
+                <div class='noticias-imagem'>
+                    <img class='img-noticia' src='https://cdn.ome.lt/rMfE0BrA8n9jNU4fpI4Lig40p8A=/480x360/smart/filer_public/88/64/886430f8-1cb3-4ed3-b63f-574b7e06707f/spider_man_no_way_home_trailer_2.jpg'/>
+                </div>
+                <div class='noticias-cabecalho'>
+                    <h2 class='noticia-titulo'>‘Homem-Aranha: Sem Volta Para Casa’ ganha trailer faltando um mês para estreia</h2>
+                    <p class='noticia-descricao'>Para decepção de muitos, nada das versões anteriores do Homem-Aranha, interpretadas por...<p>
+                    <p class='leia-mais'>Leia mais...&rarr;</p>
+                </div>
+            </div>
+        </a>
+        <!-- fim noticia testa para definir o estilo css -->
+
+        <!-- noticia testa para definir o estilo css -->
+        <a class='noticia-caixa'href='#'>
+            <div class='main-noticias'>
+                <div class='noticias-imagem'>
+                    <img class='img-noticia' src='https://cdn.ome.lt/rMfE0BrA8n9jNU4fpI4Lig40p8A=/480x360/smart/filer_public/88/64/886430f8-1cb3-4ed3-b63f-574b7e06707f/spider_man_no_way_home_trailer_2.jpg'/>
+                </div>
+                <div class='noticias-cabecalho'>
+                    <h2 class='noticia-titulo'>‘Homem-Aranha: Sem Volta Para Casa’ ganha trailer faltando um mês para estreia</h2>
+                    <p class='noticia-descricao'>Para decepção de muitos, nada das versões anteriores do Homem-Aranha, interpretadas por...<p>
+                    <p class='leia-mais'>Leia mais...&rarr;</p>
+                </div>
+            </div>
+        </a>
+        <!-- fim noticia testa para definir o estilo css -->
+
     </main>
 
     <!-- Footer -->
     <?php require_once(dirname(__DIR__) . '/php/components/footer.html'); ?>
+    <!-- News List movido do main para o footer -->
+    <!-- <?php require_once(dirname(__DIR__) . '/php/components/newsList.php'); ?> -->
 </body>
 </html>
